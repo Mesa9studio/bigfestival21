7dfb4467c18d04bc7f9399919f3ff3540390381f
JHDsBR <Jesse2013.jsc@gmail.com>
2022-07-18 17:21:41 -0300

    Detecção  de colisão + Sistema de Tags

---
+++ Big Festival Game/Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+public class Tags
+{
+    public static string    Bruxa               = "Player",
+                            Zombie              = "Zombie",
+                            NecromanticShoot    = "NecromanticShoot",
+                            NewTag              = "NewTag";
+}
+
