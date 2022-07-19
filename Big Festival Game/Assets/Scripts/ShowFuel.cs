6389ab83a916065ae35869f8b8b1d042d050ca60
JHDsBR <Jesse2013.jsc@gmail.com>
2022-07-18 21:04:45 -0300

    Mostrar combustível corretamente

--- Big Festival Game/Assets/Scripts/ShowFuel.cs
+++ Big Festival Game/Assets/Scripts/ShowFuel.cs
@@ -7,15 +7,17 @@ public class ShowFuel : MonoBehaviour
 {
     public Image img;
     public Bruxa bruxa;
+    private float maxFuel;
     // Start is called before the first frame update
     void Start()
     {
         bruxa = GameObject.FindWithTag("Player").GetComponent<Bruxa>();
+        maxFuel = bruxa.flyState.flyFuel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        img.fillAmount = bruxa.flyState.flyFuel/10;
+        img.fillAmount = bruxa.flyState.flyFuel/maxFuel;
     }
 }
